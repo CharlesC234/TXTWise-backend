38d5e624ff5ccbd6dba766fa6c89f6e17e984d80
Charles Cahill <99014706+CharlesC234@users.noreply.github.com>
2025-03-14 10:06:47 -0400

    samesite cookies set to lax

--- routes/auth.js
+++ routes/auth.js
@@ -118,7 +118,7 @@ router.post(
           res.cookie('token', token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: 'Strict',
+            sameSite: 'Lax',
             maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
           });
 
