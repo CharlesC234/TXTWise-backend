3aac40fbb52667603ec39045f9e6c243becfc9c5
Charles Cahill <99014706+CharlesC234@users.noreply.github.com>
2025-03-14 10:24:07 -0400

    secure cookies?

--- routes/auth.js
+++ routes/auth.js
@@ -117,7 +117,7 @@ router.post(
           // Set JWT in HTTP-Only Cookie
           res.cookie('token', token, {
             httpOnly: true,
-            secure: false,
+            secure: true,
             sameSite: 'None',
             maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
           });
