eee69b64dcce261e4218bc770c0d1aa0bc26a7d0
Charles Cahill <99014706+CharlesC234@users.noreply.github.com>
2025-03-14 10:09:42 -0400

    cookies

--- routes/auth.js
+++ routes/auth.js
@@ -117,7 +117,7 @@ router.post(
           // Set JWT in HTTP-Only Cookie
           res.cookie('token', token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
+            secure: true,
             sameSite: 'Lax',
             maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
           });
